55fec0b13c4a0cd8791b8b4f478f9e3badf42c0d
David_tyr <hestionplayer@gmail.com>
2025-09-25 19:02:11 -0500

    Proyecto en blanco

---
+++ AgroNet-B/src/main/java/com/tyrservices/agronetb/AgroNetBApplication.java
@@ -0,0 +1,13 @@
+package com.tyrservices.agronetb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroNetBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroNetBApplication.class, args);
+    }
+
+}
---
+++ AgroNet-B/src/test/java/com/tyrservices/agronetb/AgroNetBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrservices.agronetb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroNetBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
